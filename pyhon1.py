807a1fbe30eb49dbd56b358a51fa866e882604e6
ushivirtta786-ui <ushivirtta786@gmail.com>
2026-02-12 13:46:34 +0530

    Add files via upload

---
+++ pyhon1.py
@@ -0,0 +1,26 @@
+# Mass=float(input('Entre a='))
+# Velocity=float(input('Entre b='))
+# Momentum = Mass*Velocity
+# print('Momentum is= ', Momentum)
+
+# a= float(input('A='))
+# b= float(input('B='))
+# print('add = ', a+b)
+# print('sub = ', a-b)
+# print('Div = ', a/b)
+# print('modulos =', a%b)
+
+# import math
+# c=float(input('c='))
+# if 0<c<9:
+#     print('C*C',c*c)
+
+# elif 0<c<99:
+#         print(c**(1/2))
+
+# elif 0<c<999:
+#             print(c*c*c)
+
+num=int(input('A='))
+reverse_num = int(str(num)[::-1])
+print('A=', reverse_num)
