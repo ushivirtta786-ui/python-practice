bdcc233f41adc3fa2bc11f7f4bb95543571868dd
ushivirtta786-ui <ushivirtta786@gmail.com>
2026-02-14 15:50:46 +0530

    Add files via upload

--- pyhon1.py
+++ pyhon1.py
@@ -1,25 +1,25 @@
-# Mass=float(input('Entre a='))
-# Velocity=float(input('Entre b='))
-# Momentum = Mass*Velocity
-# print('Momentum is= ', Momentum)
+Mass=float(input('Entre a='))
+Velocity=float(input('Entre b='))
+Momentum = Mass*Velocity
+print('Momentum is= ', Momentum)
 
-# a= float(input('A='))
-# b= float(input('B='))
-# print('add = ', a+b)
-# print('sub = ', a-b)
-# print('Div = ', a/b)
-# print('modulos =', a%b)
+a= float(input('A='))
+b= float(input('B='))
+print('add = ', a+b)
+print('sub = ', a-b)
+print('Div = ', a/b)
+print('modulos =', a%b)
 
-# import math
-# c=float(input('c='))
-# if 0<c<9:
-#     print('C*C',c*c)
+import math
+c=float(input('c='))
+if 0<c<9:
+    print('C*C',c*c)
 
-# elif 0<c<99:
-#         print(c**(1/2))
+elif 0<c<99:
+        print(c**(1/2))
 
-# elif 0<c<999:
-#             print(c*c*c)
+elif 0<c<999:
+            print(c*c*c)
 
 num=int(input('A='))
 reverse_num = int(str(num)[::-1])
