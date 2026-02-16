e37136e96e7c9433f91d6e061127c580a6f9886d
ushivirtta786-ui <ushivirtta786@gmail.com>
2026-02-16 11:55:49 +0530

    Add age calculation and currency conversion functions

--- pyhon1.py
+++ pyhon1.py
@@ -24,3 +24,19 @@ elif 0<c<999:
 num=int(input('A='))
 reverse_num = int(str(num)[::-1])
 print('A=', reverse_num)
+
+from datetime import datetime
+
+def age(dob):
+    dob = datetime.strptime(dob, "%Y-%m-%d")
+    today = datetime.today()
+    return today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+
+def inr_to_usd(slalary):
+    return salary / 74.85
+
+dob = input("Birthdate (YYYY-MM-DD): ")
+salary = float(input("Salary in INR: "))
+
+print("Age:", age(dob))
+print("Salary in USD:", round(inr_to_usd(salary), 2))
